fda2743cb3b54fa4ada1b27ae259d8a86e41a469
Brad Davis <brad.davis@kitware.com>
2011-07-23 19:51:53 -0400

    add fast conversion for triangle only polydata to vesTriangleData
    Added a new method to vesPolyDataToTriangleData to convert
    an existing vtkPolyData to an existing vesTriangleData.
    
    Speedup 1: reuse vesTriangleData instance
    Speedup 2: rewrote vertex and cell copies for speed
    
    For some surface data with ~50000 vertices, I measured an ~4.5x
    speedup using this new function.
    
    As noted in comments, these speedups could be applied to a more
    general function (that handles quads, lines, strips, etc.).  E.g.,
    convert could take two parameters and not return a new pointer.

--- src/ves/vesPolyDataToTriangleData.cpp
+++ src/ves/vesPolyDataToTriangleData.cpp
@@ -7,6 +7,57 @@
 #include "vtkPointData.h"
 #include "vtkPolyData.h"
 
+void vesPolyDataToTriangleData::ConvertTriangles(vtkPolyData* input, vesTriangleData* output)
+{
+  if (!input || !output)
+  {
+    return;
+  }
+  
+  // copy points in place to ves structure
+  output->GetPoints().resize(input->GetNumberOfPoints());
+  vtkVertex3f* v = &output->GetPoints()[0];
+  double inPoint[3];
+	for (int i = 0; i < input->GetNumberOfPoints(); ++i, ++v){
+    input->GetPoint(i, inPoint);
+		v->point[0] = inPoint[0];
+		v->point[1] = inPoint[1];
+		v->point[2] = inPoint[2];
+	}
+  
+  // copy triangles in place to ves structure
+  vtkCellArray* polys = input->GetPolys();
+  vtkIdType num;
+  vtkIdType* vertices;
+  output->GetTriangles().resize(polys->GetNumberOfCells());
+  unsigned short* outIndex = &output->GetTriangles()[0][0];
+  for (int i = 0; i < polys->GetNumberOfCells(); ++i) 
+  {
+    // there are 4 elements for each triangle cell in the array (count, i1, i2, i3)
+    polys->GetCell(4*i, num, vertices);
+    *outIndex++ = vertices[0];
+    *outIndex++ = vertices[1];
+    *outIndex++ = vertices[2];
+  }
+
+  if (input->GetPointData()->GetNormals()) 
+  {
+    vtkDataArray* normals = input->GetPointData()->GetNormals();
+    v = &output->GetPoints()[0];
+    for (int i = 0; i < input->GetNumberOfPoints(); ++i, ++v) 
+    {
+      v->normal[0] = normals->GetTuple(i)[0];
+      v->normal[1] = normals->GetTuple(i)[1];
+      v->normal[2] = normals->GetTuple(i)[2];
+    }
+  } 
+  else 
+  {
+    output->SetHasNormals(false);
+    output->ComputeNormals();
+  }
+}
+
 vesTriangleData* vesPolyDataToTriangleData::Convert(vtkPolyData* input)
 {
     //cerr << "starting conversion" << endl;
--- src/ves/vesPolyDataToTriangleData.h
+++ src/ves/vesPolyDataToTriangleData.h
@@ -6,5 +6,14 @@ class vesTriangleData;
 class vesPolyDataToTriangleData
 {
 public:
-    static vesTriangleData* Convert(vtkPolyData* input);
+  static vesTriangleData* Convert(vtkPolyData* input);
+  static void Convert(vtkPolyData* input, vesTriangleData* output);
+  
+  // This function is designed to be as fast as possible at the expense of 
+  // generality.  It only converts triangle data.  It takes about 0.2 of the time
+  // as the general ConvertFunction.
+  //
+  // Note: many of the optimizations here could be added to the Convert function
+  //       this would be wortwhile future work.
+  static void ConvertTriangles(vtkPolyData* input, vesTriangleData* output);
 };
