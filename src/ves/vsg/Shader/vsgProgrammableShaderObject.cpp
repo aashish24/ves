36e6285125fa4f3faaa93461593d89637e383d33
Nikhil Shetty <nikhil.shetty@kitware.com>
2011-08-01 08:44:19 -0400

    Bringing in vsg/Shader/vsgProgrammableShaderObject.*

--- src/ves/vsg/Shader/vsgProgrammableShaderObject.cpp
+++ src/ves/vsg/Shader/vsgProgrammableShaderObject.cpp
@@ -1,42 +1,30 @@
-/*=========================================================================
-
-  Program:   Visualization Toolkit
-  Module:    vsgProgrammableShaderObject.cxx
-
-  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
-  All rights reserved.
-  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
-
-     This software is distributed WITHOUT ANY WARRANTY; without even
-     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-     PURPOSE.  See the above copyright notice for more information.
-
-=========================================================================*/
-#include "vsgProgrammableShaderObject.h"
-
+// ============================================================================
+/**
+ * @file   vsgProgrammableShaderObject.cpp
+ *
+ * @section COPYRIGHT
+ *
+ * Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+ * All rights reserved.
+ * See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+ *
+ *   This software is distributed WITHOUT ANY WARRANTY; without even
+ *   the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ *   PURPOSE.  See the above copyright notice for more information.
+ *
+ * @author nikhil shetty <nikhil.shetty@kitware.com>
+ */
+// ============================================================================
+# include "vsg/Shader/vsgProgrammableShaderObject.h"
 // --------------------------------------------------------------------includes
-
-// -----------------------------------------------------------------------macro
-
-// --------------------------------------------------------------------internal
-// IMPORTANT: Make sure that this struct has no pointers.  All pointers should
-// be put in the class declaration. For all newly defined pointers make sure to
-// update constructor and destructor methods.
-struct vsgProgrammableShaderObjectInternal
-{
-  double value; // sample
-};
 
 // -----------------------------------------------------------------------cnstr
 vsgProgrammableShaderObject::vsgProgrammableShaderObject()
 {
-  this->Internal = new vsgProgrammableShaderObjectInternal();
 }
 
 // -----------------------------------------------------------------------destr
 vsgProgrammableShaderObject::~vsgProgrammableShaderObject()
 {
-  delete this->Internal;
 }
-
 
--- src/ves/vsg/Shader/vsgProgrammableShaderObject.h
+++ src/ves/vsg/Shader/vsgProgrammableShaderObject.h
@@ -1,47 +1,63 @@
-/*=========================================================================
-
-  Program:   Visualization Toolkit
-  Module:    vsgProgrammableShaderObject.h
-
-  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
-  All rights reserved.
-  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
-
-     This software is distributed WITHOUT ANY WARRANTY; without even
-     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-     PURPOSE.  See the above copyright notice for more information.
-
-=========================================================================*/
-// .NAME vsgProgrammableShaderObject - Abstract base type for all nodes that specify arbitary fields for interfacing with per-object attribute values
-// .SECTION Description
-// vsgProgrammableShaderObject concrete node instance can be used to program behavior for a shader in a scene.
-
+// ============================================================================
+/**
+ * @file   vsgProgrammableShaderObject.h
+ *
+ * @section COPYRIGHT
+ *
+ * Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+ * All rights reserved.
+ * See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+ *
+ *   This software is distributed WITHOUT ANY WARRANTY; without even
+ *   the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ *   PURPOSE.  See the above copyright notice for more information.
+ *
+ * @section DESCRIPTION
+ *
+ * This abstract node type is the base type for all node types that specify
+ * arbitrary fields for interfacing with per-object attribute values.
+ *
+ * A concrete vsgProgrammableShaderObject node instance is used to program
+ * behaviour for a shader in a scene. The shader is able to receive and process
+ * events that are sent to it. Each event that can be received shall be declared
+ * in the shader node using the same field syntax as is used in a prototype
+ * definition:
+ *
+ * inputOnly type name
+ *
+ * The type can be any of the standard fields. The name shall be an identifier
+ * that is unique for this shader node and is used to map the value to the
+ * shader program's uniform variable of the same name. If a shader program does
+ * not have a matching uniform variable, the field value is ignored.
+ *
+ * OutputOnly fields are not required to generate output events from a
+ * shader. Current hardware shader technology does not support this capability,
+ * though future versions may.
+ *
+ * It is recommended that user-defined field or event names defined in shader
+ * nodes follow the naming conventions described in Part 2 of ISO/IEC 19775.
+ *
+ * @author nikhil shetty <nikhil.shetty@kitware.com>
+ */
+// ============================================================================
 #ifndef __vsgProgrammableShaderObject_h
 #define __vsgProgrammableShaderObject_h
 // --------------------------------------------------------------------includes
+#include "vsg/Utility/vsgMacro.h"
+#include "vsg/Utility/vsgTypes.h"
 
-// -----------------------------------------------------------------pre-defines
-class vsgProgrammableShaderObjectInternal;
+// -------------------------------------------------------------pre-defines
 
-// -----------------------------------------------------------------------class
+// -------------------------------------------------------------------class
 class vsgProgrammableShaderObject
 {
 public:
-  // ............................................................public-methods
+  // ........................................................public-methods
   vsgProgrammableShaderObject();
-  ~vsgProgrammableShaderObject();
+  virtual ~vsgProgrammableShaderObject();
 
 protected:
-  // ...........................................................protected-ivars
-
-protected:
-//BTX
-  // .......................................................................BTX
-
-private:
-  vsgProgrammableShaderObjectInternal *Internal;
-//ETX
-  // .......................................................................ETX
+  // .......................................................protected-ivars
 };
 
 #endif // __vsgProgrammableShaderObject_h
