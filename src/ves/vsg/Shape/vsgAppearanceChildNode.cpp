5dd7d3a03dc487dc6d7fde5ee15c173761da8f06
Nikhil Shetty <nikhil.shetty@kitware.com>
2011-08-01 09:12:17 -0400

    Bringing in vsg/Shape/vsgAppearanceChildNode.*

--- src/ves/vsg/Shape/vsgAppearanceChildNode.cpp
+++ src/ves/vsg/Shape/vsgAppearanceChildNode.cpp
@@ -1,41 +1,33 @@
-/*=========================================================================
-
-  Program:   Visualization Toolkit
-  Module:    vsgAppearanceChildNode.cxx
-
-  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
-  All rights reserved.
-  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
-
-     This software is distributed WITHOUT ANY WARRANTY; without even
-     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-     PURPOSE.  See the above copyright notice for more information.
-
-=========================================================================*/
-#include "vsgAppearanceChildNode.h"
-
+// ============================================================================
+/**
+ * @file   vsgAppearanceChildNode.cpp
+ *
+ * @section COPYRIGHT
+ *
+ * Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+ * All rights reserved.
+ * See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+ *
+ *   This software is distributed WITHOUT ANY WARRANTY; without even
+ *   the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ *   PURPOSE.  See the above copyright notice for more information.
+ *
+ * @author nikhil shetty <nikhil.shetty@kitware.com>
+ */
+// ============================================================================
+# include "vsgAppearanceChildNode.h"
 // --------------------------------------------------------------------includes
 
-// -----------------------------------------------------------------------macro
+    // -------------------------------------------------------------------macro
 
-// --------------------------------------------------------------------internal
-// IMPORTANT: Make sure that this struct has no pointers.  All pointers should
-// be put in the class declaration. For all newly defined pointers make sure to
-// update constructor and destructor methods.
-struct vsgAppearanceChildNodeInternal
-{
-  double value; // sample
-};
+    // -------------------------------------------------------------------cnstr
+    vsgAppearanceChildNode::vsgAppearanceChildNode()
+       {
+       }
 
-// -----------------------------------------------------------------------cnstr
-vsgAppearanceChildNode::vsgAppearanceChildNode()
-{
-  this->Internal = new vsgAppearanceChildNodeInternal();
-}
-
-// -----------------------------------------------------------------------destr
-vsgAppearanceChildNode::~vsgAppearanceChildNode()
-{
-  delete this->Internal;
-}
-
+    // -------------------------------------------------------------------destr
+    vsgAppearanceChildNode::~vsgAppearanceChildNode()
+       {
+       }
+    // ------------------------------------------------------------------public
+    // -----------------------------------------------------------------private
--- src/ves/vsg/Shape/vsgAppearanceChildNode.h
+++ src/ves/vsg/Shape/vsgAppearanceChildNode.h
@@ -1,48 +1,43 @@
-/*=========================================================================
-
-  Program:   Visualization Toolkit
-  Module:    vsgAppearanceChildNode.h
-
-  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
-  All rights reserved.
-  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
-
-     This software is distributed WITHOUT ANY WARRANTY; without even
-     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-     PURPOSE.  See the above copyright notice for more information.
-
-=========================================================================*/
-// .NAME vsgAppearanceChildNode - Base node for all child nodes of vsgAppearanceNode type
-// .SECTION Description
-// vsgAppearanceChildNode Base node for all child nodes of vsgAppearanceNode type.
-
+// ============================================================================
+/**
+ * @file   vsgAppearanceChildNode.h
+ *
+ * @section COPYRIGHT
+ *
+ * Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+ * All rights reserved.
+ * See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+ *
+ *   This software is distributed WITHOUT ANY WARRANTY; without even
+ *   the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ *   PURPOSE.  See the above copyright notice for more information.
+ *
+ * @section DESCRIPTION
+ *
+ * This is the base node type for the child nodes of the X3DAppearanceNode type.
+ *
+ * @author nikhil shetty <nikhil.shetty@kitware.com>
+ */
+// ============================================================================
 #ifndef __vsgAppearanceChildNode_h
 #define __vsgAppearanceChildNode_h
 // --------------------------------------------------------------------includes
-#include "vsgNode.h"
+# include "vsgMacro.h"
+# include "vsgTypes.h"
+# include "vsgNode.h"
 
-// -----------------------------------------------------------------pre-defines
-class vsgAppearanceChildNodeInternal;
+    // -------------------------------------------------------------pre-defines
 
-// -----------------------------------------------------------------------class
-class vsgAppearanceChildNode : public vsgNode
-{
-public:
-  // ............................................................public-methods
-  vsgAppearanceChildNode();
-  ~vsgAppearanceChildNode();
+    // -------------------------------------------------------------------class
+    class vsgAppearanceChildNode: public vsgNode
+    {
+      public:
+      // ........................................................public-methods
+      vsgAppearanceChildNode();
+      virtual ~vsgAppearanceChildNode();
 
-protected:
-  // ...........................................................protected-ivars
+      protected:
+      // .......................................................protected-ivars
 
-protected:
-//BTX
-  // .......................................................................BTX
-
-private:
-  vsgAppearanceChildNodeInternal *Internal;
-//ETX
-  // .......................................................................ETX
-};
-
+    };
 #endif // __vsgAppearanceChildNode_h
