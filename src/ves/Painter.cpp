704834b5fe0ae7ae1fff947a959c2f6a4de96600
Pat Marion <pat.marion@kitware.com>
2011-08-26 14:31:11 -0400

    Do not enable backface culling by default

--- src/ves/Painter.cpp
+++ src/ves/Painter.cpp
@@ -191,8 +191,6 @@ void Painter::visitShape(vsg::Shape* shape)
   // Clear the buffers
   //  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   //  glEnable(GL_DEPTH_TEST);
-  glEnable(GL_CULL_FACE);
-  glCullFace(GL_BACK);
   glEnable(GL_BLEND);
   glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
