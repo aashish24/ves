d3e51a0d3a2a810f03b5a695a66f7e7b4d672eae
Pat Marion <pat.marion@kitware.com>
2012-01-13 14:40:55 -0500

    add new documentation pages to doxygen
    This also adds the vtk doxygen tag to the build process.

---
+++ Docs/Doxygen/main_page.h
@@ -0,0 +1,53 @@
+/** \mainpage VES Documentation
+
+
+<ul>
+        <li><a href="classes.html">Class and Function Documentation</a></li>
+        <li>\ref repository</li>
+        <li>\ref installation</li>
+        <li>\ref bugs</li>
+        <li>\ref online_resources</li>
+        <li>\ref license</li>
+
+</ul>
+
+
+
+\page bugs Reporting Bugs and Making Suggestions
+
+Please report bugs with the <a href="http://public.kitware.com/Bug">mantis bug tracker</a>.
+When asked to choose a project, select <b>VES</b>. You can also report issues and
+make suggestions on the public mailing list <i>ves@public.kitware.com</i>.
+See \ref online_resources for a link to the mailing list subscription page.
+
+
+
+\page repository VES Source Code Repository
+
+The VES source code repository is managed via git, and can be obtained from
+
+git://vtk.org/VES.git
+
+See \ref installation for further details on how to build and install VES.
+
+
+
+\page online_resources Online Resources
+
+The following online resources are available for VES users and developers:
+
+- The VES home page and wiki: http://vtk.org/Wiki/VES
+- The VES mailing list: ves@public.kitware.com subscribe at http://public.kitware.com/cgi-bin/mailman/listinfo/ves
+- The VES source code repository: git://vtk.org/VES.git
+- The VES reference API documentation: http://public.kitware.com/ves-docs
+- The VES software quality dashboard: http://www.cdash.org/CDash/index.php?project=VES
+- The VES bug tracker: http://public.kitware.com/Bug
+
+
+\page installation Building and Getting Started
+
+Please see the <a href="http://vtk.org/Wiki/VES_Developers_Guide">VES Developers Guide</a>
+for download, build, and install instructions.
+
+
+*/
