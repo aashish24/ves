a871b2173a985d2f1fdec6d179bb66fbf08ee8ad
Pat Marion <pat.marion@kitware.com>
2012-01-15 11:30:02 -0500

    kiwi android: add missing assets.cmake file

---
+++ Apps/Android/PointCloud/assets.cmake
@@ -0,0 +1,10 @@
+set(assets
+  ../../iOS/Kiwi/Kiwi/Data/cturtle.vtk
+  ../../../src/shaders/vesShader_vert.glsl
+  ../../../src/shaders/vesShader_frag.glsl
+  )
+
+foreach(f ${assets})
+  get_filename_component(fname ${f} NAME)
+  configure_file(${f} ${CMAKE_BINARY_DIR}/assets/${fname} COPYONLY)
+endforeach()
