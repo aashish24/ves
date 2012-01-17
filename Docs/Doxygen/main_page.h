be4dfb185fa7b830ca548431072410ea9ae6210e
Pat Marion <pat.marion@kitware.com>
2012-01-17 01:12:03 -0500

    update links to VES wiki in doxygen main page documentation

--- Docs/Doxygen/main_page.h
+++ Docs/Doxygen/main_page.h
@@ -2,13 +2,12 @@
 
 
 <ul>
-        <li><a href="classes.html">API Documentation</a></li>
-        <li>\ref repository</li>
-        <li>\ref installation</li>
-        <li>\ref bugs</li>
-        <li>\ref online_resources</li>
-        <li>\ref license</li>
-
+  <li><a href="classes.html">API Documentation</a></li>
+  <li><a href="http://vtk.org/Wiki/VES">VES Homepage</a>
+  <li><a href="http://vtk.org/Wiki/VES/Download_And_Build_Instructions">Download & Build Instructions</a></li>
+  <li>\ref bugs</li>
+  <li>\ref online_resources</li>
+  <li>\ref license</li>
 </ul>
 
 
@@ -22,16 +21,6 @@ See \ref online_resources for a link to the mailing list subscription page.
 
 
 
-\page repository VES Source Code Repository
-
-The VES source code repository is managed via git, and can be obtained from
-
-git://vtk.org/VES.git
-
-See \ref installation for further details on how to build and install VES.
-
-
-
 \page online_resources Online Resources
 
 The following online resources are available for VES users and developers:
@@ -39,15 +28,10 @@ The following online resources are available for VES users and developers:
 - The VES home page and wiki: http://vtk.org/Wiki/VES
 - The VES mailing list: ves@public.kitware.com subscribe at http://public.kitware.com/cgi-bin/mailman/listinfo/ves
 - The VES source code repository: git://vtk.org/VES.git
-- The VES reference API documentation: http://public.kitware.com/ves-docs
+- The VES API documentation: http://public.kitware.com/ves-docs
 - The VES software quality dashboard: http://www.cdash.org/CDash/index.php?project=VES
 - The VES bug tracker: http://public.kitware.com/Bug
 
-
-\page installation Building and Getting Started
-
-Please see the <a href="http://vtk.org/Wiki/VES_Developers_Guide">VES Developers Guide</a>
-for download, build, and install instructions.
 
 
 */
