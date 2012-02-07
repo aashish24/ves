b533364d1e57d96e16b050e664ae246889f3280a
Jeffrey Baumes <jeff.baumes@kitware.com>
2012-02-07 13:07:22 -0500

    Adding Willow Garage copyright to files edited in that work

--- src/ves/vesCamera.cpp
+++ src/ves/vesCamera.cpp
@@ -4,6 +4,7 @@
       http://www.kitware.com/ves
 
   Copyright 2011 Kitware, Inc.
+  Copyright 2012 Willow Garage, Inc.
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
