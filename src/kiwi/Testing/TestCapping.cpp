a0a3784fb5d19e9b30e2d6f0ab62e544600ed215
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2012-04-24 18:59:16 -0400

    Added baseline

--- src/kiwi/Testing/TestCapping.cpp
+++ src/kiwi/Testing/TestCapping.cpp
@@ -182,7 +182,7 @@ void LoadDefaultData()
 bool DoTesting()
 {
   const double threshold = 10.0;
-  const std::string testName = "Clipped Standford Bunny";
+  const std::string testName = "Capped Standford Bunny";
 
   vesKiwiBaselineImageTester baselineTester;
   baselineTester.setApp(testHelper->app());
