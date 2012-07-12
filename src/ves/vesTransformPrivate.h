e30e0d50f63a5333b8d45b4e511be0bda6dc4d0a
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2012-07-11 23:37:18 -0400

    Some more style fixes

--- src/ves/vesTransformPrivate.h
+++ src/ves/vesTransformPrivate.h
@@ -3,41 +3,48 @@
 
 class vesTransformPrivate
 {
-private:
-  vesMatrix4x4f T,C,R,SR,S,SRinv,Cinv;
-
 public:
   vesMatrix4x4f eval()
   {
-    return T * C * R * SR * S * SRinv * Cinv;
+    return m_translation * m_center * m_rotation * m_scaleOrientation
+      * m_scale * m_scaleOrientationInv * m_centerInv;
   }
 
   void setTranslation(vesVector3f trans)
   {
-    T = makeTranslationMatrix4x4(trans);
+    m_translation = makeTranslationMatrix4x4(trans);
   }
 
   void setCenter(vesVector3f center)
   {
-    C = makeTranslationMatrix4x4(center);
-    Cinv = makeInverseMatrix4x4(C);
+    m_center = makeTranslationMatrix4x4(center);
+    m_centerInv = makeInverseMatrix4x4(m_center);
   }
 
   void setRotation(vesVector4f rot)
   {
-    R = makeRotationMatrix4x4(rot[3], rot[0], rot[1], rot[2]);
+    m_rotation = makeRotationMatrix4x4(rot[3], rot[0], rot[1], rot[2]);
   }
 
   void setScaleOrientation(vesVector4f sr)
   {
-    SR = makeRotationMatrix4x4(sr[3], sr[0], sr[1], sr[2]);
-    SRinv = makeInverseMatrix4x4(SR);
+    m_scaleOrientation = makeRotationMatrix4x4(sr[3], sr[0], sr[1], sr[2]);
+    m_scaleOrientationInv = makeInverseMatrix4x4(m_scaleOrientation);
   }
 
   void setScale(vesVector3f scale)
   {
-    S = makeScaleMatrix4x4(scale[0],scale[1],scale[2]);
+    m_scale = makeScaleMatrix4x4(scale[0],scale[1],scale[2]);
   }
+
+private:
+  vesMatrix4x4f m_translation;
+  vesMatrix4x4f m_center;
+  vesMatrix4x4f m_centerInv;
+  vesMatrix4x4f m_rotation;
+  vesMatrix4x4f m_scale;
+  vesMatrix4x4f m_scaleOrientation;
+  vesMatrix4x4f m_scaleOrientationInv;
 };
 
 #endif // VESTRANSFORMPRIVATE_H
