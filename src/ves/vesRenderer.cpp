e44372c7eabc2650bcaa9b1d7ba7b68f81e4fe46
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2013-02-01 13:33:05 -0500

    Enabled lighting and color material

--- src/ves/vesRenderer.cpp
+++ src/ves/vesRenderer.cpp
@@ -60,8 +60,23 @@ void vesRenderer::render()
   // By default enable depth test.
   glEnable(GL_DEPTH_TEST);
 
+  // TODO Hardcoded values right now
   glEnable(GL_LIGHTING);
   glEnable(GL_LIGHT0);
+
+  const GLfloat light0Ambient[] = {0.05, 0.05, 0.05, 1.0};
+  glLightfv(GL_LIGHT0, GL_AMBIENT, light0Ambient);
+
+  const GLfloat light0Diffuse[] = {1.0, 1.0, 1.0, 1.0};
+  glLightfv(GL_LIGHT0, GL_DIFFUSE, light0Diffuse);
+
+  const GLfloat light0Specular[] = {0.5, 0.5, 0.5, 1.0};
+  glLightfv(GL_LIGHT0, GL_SPECULAR, light0Specular);
+
+  const GLfloat light0Position[] = {0.0, 100.0, 100.0, 0.0};
+  glLightfv(GL_LIGHT0, GL_POSITION, light0Position);
+
+  glEnable(GL_COLOR_MATERIAL);
 
   if (this->m_sceneRoot) {
 
