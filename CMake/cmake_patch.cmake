4375118f502942840794100a7e10450fe6686055
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2013-02-06 17:34:03 -0500

    Work around for CMake/qcc bug
    Refer: http://cmake.org/gitweb?p=cmake.git;a=commitdiff;h=10e8b2da

--- CMake/cmake_patch.cmake
+++ CMake/cmake_patch.cmake
@@ -1,4 +1,5 @@
-if("${CMAKE_C_PLATFORM_ID}" STREQUAL "QNX")
+if("${CMAKE_CXX_COMPILER}" MATCHES "qcc")
+  message("Setting null")
   set(CMAKE_C_IMPLICIT_LINK_LIBRARIES "")
   set(CMAKE_CXX_IMPLICIT_LINK_LIBRARIES "")
 endif()
