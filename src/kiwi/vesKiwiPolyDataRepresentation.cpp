04f4abb722bdbfacd74f5a8f724b6d5ff836aab9
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2013-02-08 21:00:44 -0500

    Disabling blend for now as its not behaving right on QNX

--- src/kiwi/vesKiwiPolyDataRepresentation.cpp
+++ src/kiwi/vesKiwiPolyDataRepresentation.cpp
@@ -189,7 +189,7 @@ void vesKiwiPolyDataRepresentation::initializeWithMaterial(
   this->Internal->Actor->setMaterial(this->Internal->Material);
 
   this->Internal->Blend = vesSharedPtr<vesBlend>(new vesBlend());
-//  this->Internal->Blend->disable();
+  this->Internal->Blend->disable();
   this->Internal->Depth = vesSharedPtr<vesDepth>(new vesDepth());
 
   this->Internal->Actor->material()->addAttribute(this->Internal->Blend);
