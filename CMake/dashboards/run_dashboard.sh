2388035b250db8c7bd4912847e09c2b1943392a3
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2013-03-03 09:41:44 -0500

    Removed unused script

--- CMake/dashboards/run_dashboard.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-ctest=$(which ctest)
-${VES_SOURCE_ROOT:?"Need to set VES_SOURCE_ROOT to the VES clone directory"}
-cd ${VES_SOURCE_ROOT}/CMake/dashboards
-$ctest -S ./linux_memcheck_dashboard.cmake -VV
