c5a98de24f4ea6b8bcc6a09dd62c5f01b8b383c5
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2013-03-03 13:01:45 -0500

    Perform code converage and memcheck for krishna

--- CMake/dashboards/krishna_common.cmake
+++ CMake/dashboards/krishna_common.cmake
@@ -6,12 +6,19 @@ set(CTEST_TEST_TIMEOUT 500)
 set(ENV{DISPLAY} ":0")
 set(ENV{CTEST_DASHBOARD_ROOT} "/source/dashboards")
 
+# valgrind and gcov
+set(dashboard_do_memcheck TRUE)
+set(dashboard_do_coverage TRUE)
+
+# rest
 set(CTEST_MEMORYCHECK_COMMAND "/usr/bin/valgrind")
 set(CTEST_MEMORYCHECK_SUPPRESSIONS_FILE "${CTEST_DASHBOARD_ROOT}/supp.txt")
 set(CTEST_MEMORYCHECK_COMMAND_OPTIONS "-q --leak-check=full --num-callers=50")
 set(CTEST_COVERAGE_COMMAND "/usr/bin/gcov")
+set(gcov_cxx_args "-O0 -fprofile-arcs -ftest-coverage")
 
+# set cache
 set(dashboard_cache "
   VES_HOST_SUPERBUILD:BOOL=TRUE
-  VES_CXX_FLAGS:STRING=-Wall -Wextra
+  VES_CXX_FLAGS:STRING=-Wall -Wextra ${gcov_cxx_args}
 ")
