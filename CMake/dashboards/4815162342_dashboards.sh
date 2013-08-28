1f4713500f972e5ec1e36326578325b17dcfe182
Felix Veysseyre <felix.veysseyre@kitware.com>
2013-08-28 09:32:30 -0400

    BUILD: Dashboard on 4815162342 correction

--- CMake/dashboards/4815162342_dashboards.sh
+++ CMake/dashboards/4815162342_dashboards.sh
@@ -2,9 +2,8 @@
 
 cd $(dirname $0)
 
+git stash
 git pull
 
-export ANDROID_NDK=~/Softwares/Android-NDK/
-
-ctest -S 4815162342_next.cmake -VV
-ctest -S 4815162342_master.cmake -VV
+/opt/local/bin/ctest -S 4815162342_next.cmake -VV
+/opt/local/bin/ctest -S 4815162342_master.cmake -VV
